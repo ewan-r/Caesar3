553537ec3a9840b1422c2e0b72163400b0d7201f
Yahya SY <sy.yahya@yahoo.fr>
2022-10-06 11:52:10 +0200

    Revert "v1.0"
    This reverts commit 0acf9773cf26916f2adcafe84c1a1edfb0682a6f.

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-import pygame as pg
-from game.game import Game
+#! /usr/bin/env python3
 
+import pygame as pg
+from pygame.locals import *
+
+from view.menu import Menu
 
 def main():
+    """Main program."""
+    # the window is resizable and it is possible to put it in fullscreen
+    window = pg.display.set_mode((1600, 900), RESIZABLE, FULLSCREEN)
+    # relative path of the background image
+    background_image = pg.image.load("assets/background/insa-lubrityiii-bg.png").convert_alpha()
 
-    running = True
-    playing = True
-    pg.init()
-    pg.mixer.init() 
-    screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
-    clock = pg.time.Clock()
-
-    # implement menus
-
-    # implement game
-    game = Game(screen, clock)
-
-    while running:
-
-        # start menu goes here
-
-        while playing:
-            # game loop here
-            game.run()
-
+    game_menu = Menu(window, background_image)
+    game_menu.display_menu()
+    
 if __name__ == "__main__":
     main()
