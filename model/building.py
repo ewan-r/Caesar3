3ee0ff906934ffcb835d9c2f968fefa5089f6287
antoine-virgos <antoine@virgos.fr>
2022-12-08 08:55:27 +0100

    Remove building.py

--- model/building.py
+++
@@ -1,8 +0,0 @@
-class Building():
-    def __init__(self, title, sprites, x_position, y_position, state, level):
-        self.building_title = title
-        self.building_sprites = sprites
-        self.building_x = x_position
-        self.building_y = y_position
-        self.building_state = state
-        self.building_level = level
